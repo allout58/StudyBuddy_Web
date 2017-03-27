49c218fb51c46f79dc00735bac9803407fc78861
James Hollowell <jamesthollowell@gmail.com>
2017-03-27 19:52:02 -0400

    A bit of work on firebase_login

--- api/user/firebase_login.php
+++ api/user/firebase_login.php
@@ -7,23 +7,36 @@ require_once '../common.inc';
 use Lcobucci\JWT\Parser;
 use Lcobucci\JWT\ValidationData;
 
-$token = (new Parser())->parse((string) $_POST['jwt']);
+$token = (new Parser())->parse((string)$_POST['jwt']);
 
 $validator = new ValidationData();
 $validator->setAudience("studybuddy-66b00");
 $validator->setIssuer("https://securetoken.google.com/studybuddy-66b00");
 
-if ($token->validate($validator)) {
-    $out = array();
-    $out["valid"] = true;
-    $out["uid"] = $token->getClaim("sub");
-    echo json_encode($out);
-    // Go to the DB and try to pull up the account associated with the Firebase UID
+$out = array();
 
-    // If it doesn't exist, make one
-}
-else {
-    $out = array();
+if ($token->validate($validator)) {
+//    $out["valid"] = true;
+//    $out["uid"] = $token->getClaim("sub");
+    // Go to the DB and try to pull up the account associated with the Firebase UID
+    $prep_sel = $dbo->prepare("SELECT userID, lastLogin FROM Users WHERE firebase_uid=:uid");
+    $prep_sel->bindColumn(1, $userid);
+    $prep_sel->bindColumn(2, $lastLogin);
+    $prep_sel->bindValue(":uid", $token->getClaim("sub"));
+    $prep_sel->execute();
+    if ($prep_sel->rowCount() == 0) {
+        // If it doesn't exist, make one
+        $prep_ins = $dbo->prepare("INSERT INTO Users (firebase_uid, lastLogin) VALUES (:f_uid, NOW())");
+        $prep_ins->bindValue(":f_uid", $token->getClaim("sub"));
+        $prep_ins->execute();
+    }
+    $prep_sel->fetch();
+    $out['uid'] = $userid;
+    $out['lastLogin'] = $lastLogin;
+
+} else {
+
     $out["valid"] = false;
-    echo json_encode($out);
+
 }
+echo json_encode($out);
