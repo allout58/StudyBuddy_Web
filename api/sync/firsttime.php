5a13fdcb51804383c1011484a1bac70687599fb9
James Hollowell <jamesthollowell@gmail.com>
2017-03-29 21:11:39 -0400

    Work on the sync a bit
    Also refactored FB token checking into a function, so we can require it for all API interactions

--- api/sync/firsttime.php
+++ api/sync/firsttime.php
@@ -1,21 +1,39 @@
 <?php
 /**
- * Fetches all cars for a given user. Used to load all of a user's data the first time the log into a device
+ * Fetches all locations, sublocations, and friends for a given user. Used to load all of a user's data the first time the log into a device
  */
 
 require_once '../common.inc';
 
-$row = array();
+$uid = getFirebaseUIDFromJWT($_POST['jwt']);
 
-$sel_prep = $dbo->prepare("SELECT c.carID AS id, make, model, license, state, color, year, sort_order AS sortOrder, isDeleted, UNIX_TIMESTAMP(last_update) AS lastUpdate FROM Cars AS c INNER JOIN UsersCars AS uc ON c.carID=uc.cariD WHERE uc.userID=:uid");
-$sel_prep->bindParam(":uid", $_GET['uid']);
-$sel_prep->execute();
+if ($uid != null) {
+    $prep_sel_sub = $dbo->prepare("SELECT subID, name FROM SubLocations WHERE locationID=:lid");
+    $prep_sel_sub->bindParam(":lid", $locID);
 
-$out = array();
-while (($row = $sel_prep->fetch(PDO::FETCH_ASSOC))) {
-    $row['isDeleted'] = $row['isDeleted'] == 1;
-    $row['colorHex'] = colorIntToARGBHex(intval($row['color']));
-    unset($row['color']);
-    array_push($out, $row);
+    $out = array();
+
+    $sel_locs = $dbo->query("SELECT locationID, name, longitude, latitude, radius FROM Locations");
+    $locs = array();
+    while (($row = $sel_locs->fetch(PDO::FETCH_ASSOC))) {
+        $row["sublocs"] = array();
+        $prep_sel_sub->execute();
+        while (($r = $prep_sel_sub->fetch(PDO::FETCH_ASSOC))) {
+            array_push($row['sublocs'], $r);
+        }
+    }
+
+    $friends = array();
+    $friends['connected'] = array();
+    $friends['my_requests'] = array();
+    $friends['their_requests'] = array();
+
+
+
+    $out['locations'] = $locs;
+    $out['friends'] = $friends;
+    echo json_encode($out);
+} else {
+    http_response_code(401);
+    echo "{'error':'Bad token'}";
 }
-echo json_encode($out);
--- api/user/firebase_login.php
+++ api/user/firebase_login.php
@@ -1,42 +1,30 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 require_once '../common.inc';
 
-
-use Lcobucci\JWT\Parser;
-use Lcobucci\JWT\ValidationData;
-
-$token = (new Parser())->parse((string)$_POST['jwt']);
-
-$validator = new ValidationData();
-$validator->setAudience("studybuddy-66b00");
-$validator->setIssuer("https://securetoken.google.com/studybuddy-66b00");
-
+$uid = getFirebaseUIDFromJWT($_POST['jwt']);
 $out = array();
 
-if ($token->validate($validator)) {
+if ($uid != null) {
 //    $out["valid"] = true;
 //    $out["uid"] = $token->getClaim("sub");
     // Go to the DB and try to pull up the account associated with the Firebase UID
     $prep_sel = $dbo->prepare("SELECT userID, lastLogin FROM Users WHERE firebase_uid=:uid");
     $prep_sel->bindColumn(1, $userid);
     $prep_sel->bindColumn(2, $lastLogin);
-    $prep_sel->bindValue(":uid", $token->getClaim("sub"));
+    $prep_sel->bindValue(":uid", $uid);
     $prep_sel->execute();
     if ($prep_sel->rowCount() == 0) {
         // If it doesn't exist, make one
-        $prep_ins = $dbo->prepare("INSERT INTO Users (firebase_uid, lastLogin) VALUES (:f_uid, NOW())");
-        $prep_ins->bindValue(":f_uid", $token->getClaim("sub"));
+        $prep_ins = $dbo->prepare("INSERT INTO Users (firebase_uid, realName, lastLogin) VALUES (:f_uid, :name, NOW())");
+        $prep_ins->bindValue(":f_uid", $uid);
+        $prep_ins->bindValue(":name", $_POST['name']);
         $prep_ins->execute();
+        $prep_sel->execute();
     }
     $prep_sel->fetch();
     $out['uid'] = $userid;
     $out['lastLogin'] = $lastLogin;
-
 } else {
-
     $out["valid"] = false;
-
 }
 echo json_encode($out);
